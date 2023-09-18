e43086141689da094c8b5c228a433f0d8e484ab7
wespa4 <145107412+wespa4@users.noreply.github.com>
2023-09-19 02:03:00 +0300

    did n2 in second prc

---
+++ src/practice2/two/TestBall.java
@@ -0,0 +1,16 @@
+package practice2.two;
+
+import java.util.Scanner;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball ball = new Ball(100,100);
+        System.out.println(ball);
+        ball.move(12, 34);
+        System.out.println(ball);
+
+        Scanner sc = new Scanner(System.in);
+        ball.setXY(sc.nextInt(), sc.nextInt()); // считываем в setXY строчки с консоли
+        System.out.println(ball.getX() + " " + ball.getY());
+    }
+}
